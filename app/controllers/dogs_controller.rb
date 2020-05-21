6a86358353042de05c8c57ed0d78ecbd6782ba85
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-05-20 23:43:41 -0400

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,31 @@
 class DogsController < ApplicationController
+
+def index
+    @dogs = Dog.all
+end
+
+# def new
+
+# end
+
+# def create
+
+# end
+
+# def edit
+
+# end
+
+# def update
+
+# end
+
+def show
+    @dog = Dog.find(params[:id])
+end
+
+# def destroy
+
+# end
+
 end
